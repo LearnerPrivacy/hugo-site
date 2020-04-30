e0d6a70bfe9101dcaca9f4859ee2063a6163557c
Charles Severance <csev@umich.edu>
2020-04-30 10:30:59 -0400

    rebuilding site Thu Apr 30 10:30:55 EDT 2020

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # If a command fails then the deploy stops
-set -e
+# set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
@@ -26,7 +26,7 @@ git push origin master
 
 cd ..
 echo "Updating theme repo..."
-cd theme/lp-dream
+cd themes/lp-dream
 git add .
 git commit -m "$msg"
 git push
